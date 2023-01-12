debd6c4b08a3e11f12ba164a38e16c556ea42e0b
Dalron J Robertson <112004061+DJRobertson95@users.noreply.github.com>
2023-01-11 20:15:11 -0600

    grr

--- api/department.js
+++ api/department.js
@@ -5,27 +5,32 @@ const { getAllDepartment, getDepartmentById } = require('../db/Department')
 // Router Middleware
 const departmentRouter = express.Router();
 
-// Router Handelers
+// Router Handlers
     // GET/department
-departmentRouter.get("/", async (req, res, next) => {
-    try {
-        const department = await getAllDepartment();
-        res.send(department)
-    } catch (error) {
-        console.error(error)
-    }
-});
-
-// GET/departmentById
+    departmentRouter.get("/", async (req, res, next) => {
+        console.log("Handling GET /department");
+        try {
+            const department = await getAllDepartment();
+            res.send(department)
+        } catch (error) {
+            console.log(error);
+            res.status(500).json({
+              error: "Unable to retrieve departments"
+            });
+          }
+        });
+    
+// GET/department/:departmentId
 departmentRouter.get("/:departmentId", async (req, res, next) => {
+    console.log("Handling GET /department/:departmentId");
     try {
         const { departmentId } = req.params
         const response = await getDepartmentById(departmentId)
         res.send(response)
     } catch (error) {
-        console.error(error)
+        console.log(error)
     }
 })
 
 // Exports
-module.exports = {departmentRouter}
+module.exports = {departmentRouter};
