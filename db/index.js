1b69453b015fe8068c0d27bb4e4e1da523cd175e
Dalron J Robertson <112004061+DJRobertson95@users.noreply.github.com>
2023-01-13 17:16:10 -0600

    update

--- db/index.js
+++ db/index.js
@@ -1,9 +1,10 @@
 // 1) Imports 
 // const { Client } = require('pg');
+require('dotenv').config();
 const pg = require('pg');
 
 // 2) Now we have to establish a client connection to our DB url
-const client = new Client(process.env.DATABASE_URL || 'postgres://localhost:5432/Dal-Recipe');
+const client = new pg.Client(process.env.DATABASE_URL || 'postgres://localhost:5432/Dal-Recipe');
 
 // 3) Export this client connection that we just created
 module.exports = { client };
