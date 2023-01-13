b3e8686ebfb40d526e8427b964e648d178eced93
Dalron J Robertson <112004061+DJRobertson95@users.noreply.github.com>
2023-01-13 17:06:36 -0600

    fixing deploy

--- db/index.js
+++ db/index.js
@@ -3,7 +3,7 @@
 const pg = require('pg');
 
 // 2) Now we have to establish a client connection to our DB url
-const client = new pg.Client(`postgres://localhost:5432/Dal-Recipe`);
+const client = new Client(process.env.DATABASE_URL || 'postgres://localhost:5432/Dal-Recipe');
 
 // 3) Export this client connection that we just created
 module.exports = { client };
